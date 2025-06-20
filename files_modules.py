5757f6aa4d7bbf3447175353949e1a6f300cf6eb
Volodymyr Panchenko <volodya.ua@gmail.com>
2025-06-20 12:56:34 +0300

    work home

---
+++ files_modules.py
@@ -0,0 +1,49 @@
+def total_salary(path):
+    total = 0
+    count = 0
+    try:
+        with open(path, "r") as fh:
+            rows = [line.strip().split(",") for line in fh]
+            for row in rows:
+                if len(row) >= 2:
+                    try:
+                        total += int(row[1])
+                        count += 1
+                    except ValueError:
+                        continue  # пропускаем строки с некорректной зарплатой 
+        if count > 0:
+            average = total // count
+        else:
+            average = 0
+        return total, average
+    except FileNotFoundError:
+        print(f"File not found: {path}")
+    except IOError as e:
+        print(f"I/O error: {e}")
+    return 0, 0  # Возврат по умолчанию при ошибке
+
+# Вызов
+# total, average = total_salary("emp.txt")
+# print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+
+def get_cats_info(path):
+    cats = []
+    try:
+        with open(path, "r") as fh:
+            rows = [line.strip().split(",") for line in fh]
+            for row in rows:
+                if len(row) >= 3:
+                    try:
+                        age = int(row[2])
+                        cats.append( {"id": row[0], "name": row[1], "age":age})   
+                    except ValueError:
+                        continue  # пропускаем строки с некорректной зарплатой 
+        return cats
+    except FileNotFoundError:
+        print(f"File not found: {path}")
+    except IOError as e:
+        print(f"I/O error: {e}")
+    return 0, 0  # Возврат по умолчанию при ошибке
+
+cats_info = get_cats_info("cats_info.txt")
+print(cats_info)
